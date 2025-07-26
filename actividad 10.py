5b17b346cd9d09bb48df8d59f410cd1a5ee35b02
Usuario <hugocanizzabala@gmail.com>
2025-07-25 21:10:10 -0600

    cadena texto

--- actividad 10.py
+++ actividad 10.py
@@ -1,5 +1,18 @@
 def menu():
     print("MENU RECURSIVIDAD")
-    print("invertir una cadena a texto")
-    print("calcular la suma ede los N numeros naturales")
-    print("")
+    print("1. invertir una cadena a texto")
+    print("2. calcular la suma ede los N numeros naturales")
+    print("3. imprimir una cuenta recursiva de N hasta 1")
+    print("4. sumar los digitos de un numero")
+    print("5 contar cuantos digitos tiene un numero")
+    print("6. salir")
+def cadena_texto(cadena):
+    letras=reversed(cadena)
+    return  letras
+
+menu()
+op=int(input("ingrese opcion a ejecutar"))
+if op==1:
+    cad=input ("ingrese cadena a texto")
+    print(cadena_texto(cad))
+
